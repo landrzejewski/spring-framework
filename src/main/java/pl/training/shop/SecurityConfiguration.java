fbf557f94c8bf15dba9e6e6dfa52b14afb251875
landrzejewski <landrzejewski.poczta@gmail.com>
2022-11-18 10:46:32 +0100

    Base project

--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package pl.training.shop;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import pl.training.shop.commons.security.BaseSecurityConfiguration;
 
 @KeycloakConfiguration
@@ -16,7 +17,11 @@ public class SecurityConfiguration extends BaseSecurityConfiguration {
                 .authorizeRequests()
                 .mvcMatchers("/payments/process").hasRole("ADMIN")
                 .mvcMatchers("/api/**").hasRole("ADMIN")
-                .mvcMatchers("/**").permitAll();
+                .mvcMatchers("/**").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout.html"))
+                .logoutSuccessUrl("/index.html");
     }
 
 }
