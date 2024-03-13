c982abce8ef154bdc0a42f563d6412dd2ad32f08
landrzejewski <landrzejewski.poczta@gmail.com>
2024-03-13 11:18:24 +0100

    Update

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -1,20 +1,9 @@
 package pl.training.shop;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@EnableMongoRepositories
 //@EnableJpaRepositories(repositoryImplementationPostfix = "Impl")
 @Configuration
-public class ApplicationConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/api/*")
-               .allowedOrigins("http://localhost:4200")
-               .allowedHeaders("*")
-               .allowedMethods("GET", "POST", "PUT");
-    }
-
+public class ApplicationConfiguration {
 }
