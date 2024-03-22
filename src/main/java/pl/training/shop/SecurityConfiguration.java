90af97bf70b7bd9b48f9715800244d7fd8599e8c
landrzejewski <landrzejewski.poczta@gmail.com>
2024-03-22 15:13:22 +0100

    AOP

--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -3,10 +3,12 @@ package pl.training.shop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import pl.training.shop.security.KeycloakGrantedAuthoritiesMapper;
 import pl.training.shop.security.KeycloakJwtGrantedAuthoritiesConverter;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class SecurityConfiguration {
                 .cors(config -> config.configurationSource(request -> corsConfiguration()))
                 .csrf(config -> config.ignoringRequestMatchers("/api/**"))
                 .oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
+                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
                 .authorizeHttpRequests(config -> config
                         .anyRequest().hasRole("ADMIN")
                 )
@@ -40,6 +43,11 @@ public class SecurityConfiguration {
         var jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
         jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
+    }
+
+    // Client scopes -> Client scope details (roles) -> Mapper details -> Add to userinfo enabled (Keycloak Admin console)
+    private void userInfoCustomizer(OAuth2LoginConfigurer.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
     }
 
 }
