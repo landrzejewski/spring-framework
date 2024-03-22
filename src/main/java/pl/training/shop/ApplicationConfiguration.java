176d378660885228712c683b6b63bdd3a9641389
landrzejewski <landrzejewski.poczta@gmail.com>
2024-03-22 15:03:28 +0100

    AOP

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -1,14 +1,8 @@
 package pl.training.shop;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import pl.training.shop.security.jwt.*;
 
 //@EnableMongoRepositories
 //@EnableJpaRepositories(repositoryImplementationPostfix = "Impl")
@@ -20,16 +14,6 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
         registry.addViewController("").setViewName("index");
         registry.addViewController("index.html").setViewName("index");
         registry.addViewController("login.html").setViewName("login-form");
-    }
-
-    @Autowired
-    public void configure(AuthenticationManagerBuilder builder, UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, JwtService jwtService) {
-        var daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        builder.authenticationProvider(daoAuthenticationProvider);
-
-        builder.authenticationProvider(new JwtAuthenticationProvider(jwtService));
     }
 
 }
--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -1,10 +1,45 @@
 package pl.training.shop;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import pl.training.shop.security.KeycloakJwtGrantedAuthoritiesConverter;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
 
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        var corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4800"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+        return corsConfig;
+    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(config -> config.configurationSource(request -> corsConfiguration()))
+                .csrf(config -> config.ignoringRequestMatchers("/api/**"))
+                .oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .build();
+    }
+
+    private void jwtConfig(OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer jwtConfigurer) {
+        var jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
+        jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
+    }
 
 }
