9ed52de469d17b515356ab880777ec453c27e434
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-04-11 14:16:23 +0200

    update

--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -1,8 +1,22 @@
 package pl.training.shop;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(config -> config.ignoringRequestMatchers("/api/**"))
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .oauth2ResourceServer(config -> config.jwt(jwtConfigurer -> {}))
+                .build();
+    }
 
 }
