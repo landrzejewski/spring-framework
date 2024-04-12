d29b383ba231b91c07500cc01404b56357aec38d
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-04-12 10:12:55 +0200

    update

--- src/main/java/pl/training/Application.java
+++ src/main/java/pl/training/Application.java
@@ -1,4 +1,4 @@
-package pl.training.shop;
+package pl.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pl/training/ApplicationConfiguration.java
@@ -0,0 +1,7 @@
+package pl.training;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+}
