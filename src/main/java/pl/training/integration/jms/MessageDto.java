6368268c526b56ccfe12bde6c7d81bb6d66f7e6c
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-04-12 11:46:34 +0200

    update

---
+++ src/main/java/pl/training/integration/jms/MessageDto.java
@@ -0,0 +1,16 @@
+package pl.training.integration.jms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto implements Serializable {
+
+    private String body;
+
+}
