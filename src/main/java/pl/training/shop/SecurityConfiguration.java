bd5dc4544df29d47b1387198b8b590a8dbbb9076
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-05 16:04:36 +0200

    REST

--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -2,87 +2,25 @@ package pl.training.shop;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
-import pl.training.shop.security.AuthenticationLoggingFilter;
-import pl.training.shop.security.BasicAuthenticationEntryPoint;
-import pl.training.shop.security.RequestUrlAuthorizationManager;
-import pl.training.shop.security.jwt.JwtAuthenticationFilter;
-import pl.training.shop.security.jwt.JwtAuthenticationProvider;
-import pl.training.shop.security.jwt.JwtService;
+import pl.training.shop.security.KeycloakGrantedAuthoritiesMapper;
+import pl.training.shop.security.KeycloakJwtGrantedAuthoritiesConverter;
+import pl.training.shop.security.KeycloakLogoutHandler;
 
 import java.util.List;
 
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-// @EnableWebSecurity(debug = true)
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true/*, prePostEnabled = true*/)
 @Configuration
 public class SecurityConfiguration {
-
-   /*AuthenticationManager authenticationManager; // Interfejs/kontrakt dla procesu uwierzytelnienia użytkownika
-        ProviderManager providerManager; // Podstawowa implementacja AuthenticationManager, deleguje proces uwierzytelnienia do jednego z obiektów AuthenticationProvider
-            AuthenticationProvider authenticationProvider; // Interfejs/kontrakt dla obiektów realizujących uwierzytelnianie z wykorzystaniem konkretnego mechanizmu/implementacji
-                DaoAuthenticationProvider daoAuthenticationProvider; // Jedna z implementacji AuthenticationProvider, ładuje dane o użytkowniku wykorzystując UserDetailsService i porównuje je z tymi podanymi w czasie logowani
-                    UserDetailsService userDetailsService; // Interfejs/kontrakt usługi ładującej dane dotyczące użytkownika
-
-    UsersDetailsManager usersDetailsManager; Interfejs/kontrakt pochodny UserDetailsService, pozwalający na zarządzanie użytkownikami
-        InMemoryUserDetailsManager inMemoryUserDetailsManager; // Jedna z implementacji UsersDetailsManager, przechowuje informacje w pamięci
-
-    PasswordEncoder passwordEncoder; //Interfejs/kontrakt pozwalający na hashowanie i porównywanie haseł
-        BCryptPasswordEncoder bCryptPasswordEncoder; //Jedna z implementacji PasswordEncoder
-
-    SecurityContextHolder securityContextHolder; // Przechowuje/udostępnia SecurityContext
-        SecurityContext securityContext; // Kontener przechowujący Authentication
-            Authentication authentication; // Reprezentuje dane uwierzytelniające jak i uwierzytelnionego użytkownika/system
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken; // Jedna z implementacji Authentication, zawiera login i hasło jako credentials
-                    UserDetails userDetails; // Interfejs/kontrakt opisujący użytkownika
-                    GrantedAuthority grantedAuthority; // Interfejs/kontrakt opisujący role/uprawnienia
-                        SimpleGrantedAuthority simpleGrantedAuthority; // Jedna z implementacji SimpleGrantedAuthority
-
-    AuthorizationManager authorizationManager; // Interfejs/kontrakt dla procesu autoryzacji
-        AuthoritiesAuthorizationManager authoritiesAuthorizationManager; // Jedna z implementacji AuthorizationManager (role)*/
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public UserDetails user() {
-        return User.withUsername("jan")
-                .password(passwordEncoder().encode("123"))
-                .roles("ADMIN")
-                .authorities("read", "write")
-                .build();
-    }
-
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        return username -> {
-            if (!username.equals("jan")) {
-                throw new UsernameNotFoundException(username);
-            }
-            return user();
-        };
-    }*/
-
-    /*@Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        // return new InMemoryUserDetailsManager(user());
-        var manager = new JdbcUserDetailsManager(dataSource);
-        manager.setUsersByUsernameQuery("select username, password, enabled from users where username = ?");
-        manager.setAuthoritiesByUsernameQuery("select username, authority from authorities where username = ?");
-        return manager;
-    }*/
 
     @Bean
     public CorsConfiguration corsConfiguration() {
@@ -95,40 +33,42 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfiguration corsConfiguration,
-                                                   JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
-        return http
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class )
-                .addFilterBefore(new AuthenticationLoggingFilter(), ExceptionTranslationFilter.class)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(config -> config.configurationSource(request -> corsConfiguration()))
                 .csrf(config -> config.ignoringRequestMatchers("/api/**"))
-                .cors(config -> config.configurationSource(request -> corsConfiguration))
+                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
+                .oauth2ResourceServer(config -> config.jwt(withDefaults()))
+                //.oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
                 .authorizeHttpRequests(config -> config
-                        .requestMatchers("/api/tokens").permitAll()
-                        .requestMatchers("/login.html").permitAll()
-                        .requestMatchers(POST, "/payments/process").hasRole("ADMIN")
-                        .anyRequest().access(new RequestUrlAuthorizationManager())
-                        // .anyRequest().authenticated()
-                )
-                // .httpBasic(withDefaults())
-                .httpBasic(config -> config
-                        .realmName("Training")
-                        .authenticationEntryPoint(new BasicAuthenticationEntryPoint())
-                )
-                //.formLogin(withDefaults())
-                .formLogin(config -> config
-                        .loginPage("/login.html")
-                        .defaultSuccessUrl("/index.html")
-                        // .usernameParameter("username")
-                        // .passwordParameter("password")
-                        // .successHandler(new CustomAuthenticationSuccessHandler())
-                        // .failureHandler(new CustomAuthenticationFailureHandler())
+                        .anyRequest().hasRole("ADMIN")
                 )
                 .logout(config -> config
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout.html"))
-                        .logoutSuccessUrl("/login.html")
+                        .logoutSuccessUrl("/index.html")
                         .invalidateHttpSession(true)
+                        .addLogoutHandler(new KeycloakLogoutHandler(new RestTemplate()))
                 )
                 .build();
     }
+
+    @Bean
+    public JwtAuthenticationConverter jwtConfigurer() {
+        var jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
+        return jwtConverter;
+    }
+
+    /*private void jwtConfig(OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer jwtConfigurer) {
+        var jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
+        jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
+    }*/
+
+    // Client scopes -> Client scope details (roles) -> Mapper details -> Add to userinfo enabled (Keycloak Admin console)
+    private void userInfoCustomizer(OAuth2LoginConfigurer<HttpSecurity>.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
+    }
+
 
 }
