28fb407fbb77e0f5beadcc8025fe5e42ba8d7297
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-06 08:07:42 +0200

    REST

--- src/main/java/pl/training/Application.java
+++ src/main/java/pl/training/Application.java
@@ -3,8 +3,6 @@ package pl.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// java -jar -Dspring.profiles.active=dev  shop-1.0-SNAPSHOT.jar
-
 @SpringBootApplication
 public class Application {
 
