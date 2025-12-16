9cac10ee20cb3e4ec1a5e7ba19f76273722f67c9
Admin <landrzejewski.poczta@gmail.com>
2025-12-16 15:06:10 +0100

    update

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -1,28 +1,13 @@
 package pl.training.shop;
 
-import com.zaxxer.hikari.HikariDataSource;
-import jakarta.persistence.EntityManagerFactory;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import pl.training.shop.time.SystemTimeProvider;
 import pl.training.shop.time.TimeProvider;
 
-import javax.sql.DataSource;
-import java.util.Map;
-
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
-@PropertySource("classpath:jdbc.properties")
-@EnableJpaRepositories
-@EnableTransactionManagement
-@EnableAspectJAutoProxy
-@ComponentScan
 @Configuration
 public class ApplicationConfiguration {
 
@@ -30,34 +15,6 @@ public class ApplicationConfiguration {
     @Bean({"timeProvider", "systemTimeProvider"}) // nadpisanie nazwy/nazw
     public TimeProvider timeProvider() {
         return new SystemTimeProvider();
-    }
-
-    @Bean
-    public DataSource dataSource(Environment environment) {
-        var datasource = new HikariDataSource();
-        datasource.setUsername(environment.getProperty("database.username"));
-        datasource.setPassword(environment.getProperty("database.password"));
-        datasource.setJdbcUrl(environment.getProperty("database.url"));
-        datasource.setDriverClassName(environment.getProperty("database.driver"));
-        datasource.setMaximumPoolSize(Integer.parseInt(environment.getProperty("database.pool-size")));
-        return datasource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        var factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setPackagesToScan("pl.training.shop");
-        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        factoryBean.setJpaPropertyMap(Map.of(
-                "jakarta.persistence.schema-generation.database.action", "drop-and-create"
-        ));
-        return factoryBean;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
     }
 
 }
