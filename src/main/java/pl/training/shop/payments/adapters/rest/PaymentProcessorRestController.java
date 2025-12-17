d6d86c6f6e7caf3ee86123aa705af4f8c2fb5158
Admin <landrzejewski.poczta@gmail.com>
2025-12-17 12:06:24 +0100

    update

--- src/main/java/pl/training/shop/payments/adapters/rest/PaymentProcessorRestController.java
+++ src/main/java/pl/training/shop/payments/adapters/rest/PaymentProcessorRestController.java
@@ -1,16 +1,29 @@
 package pl.training.shop.payments.adapters.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.training.shop.commons.web.LocationUri;
+import pl.training.shop.payments.domain.PaymentProcessor;
 
 @RequestMapping("api/payments")
 @RestController
+@RequiredArgsConstructor
 public class PaymentProcessorRestController {
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello World!";
+    private final PaymentProcessor paymentProcessor;
+    private final RestPaymentModelMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<PaymentDto> process(@RequestBody PaymentRequestDto paymentRequestDto) {
+        var paymentRequest = mapper.toDomain(paymentRequestDto);
+        var payment = paymentProcessor.process(paymentRequest);
+        var locationUri = LocationUri.fromRequestWith(payment.getId());
+        return ResponseEntity.created(locationUri)
+                .body(mapper.toDto(payment));
     }
 
 }
---
+++ src/main/java/pl/training/shop/payments/adapters/rest/RestPaymentModelMapper.java
@@ -0,0 +1,25 @@
+package pl.training.shop.payments.adapters.rest;
+
+import org.javamoney.moneta.Money;
+import org.springframework.stereotype.Component;
+import pl.training.shop.payments.domain.Payment;
+import pl.training.shop.payments.domain.PaymentRequest;
+
+@Component
+public class RestPaymentModelMapper {
+
+    public PaymentRequest toDomain(PaymentRequestDto paymentRequestDto) {
+        var amount = Money.parse(paymentRequestDto.getValue());
+        return new PaymentRequest(paymentRequestDto.getId(), amount);
+    }
+
+    public PaymentDto toDto(Payment payment) {
+        var paymentDto = new PaymentDto();
+        paymentDto.setId(payment.getId());
+        paymentDto.setStatus(payment.getStatus().name());
+        paymentDto.setValue(payment.getValue().toString());
+        paymentDto.setTimestamp(payment.getTimestamp());
+        return paymentDto;
+    }
+
+}
