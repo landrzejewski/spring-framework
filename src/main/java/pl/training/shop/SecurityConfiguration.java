8dde3d0691479b916ebf2d5423473b2fb8dbaaf4
Admin <landrzejewski.poczta@gmail.com>
2025-12-19 08:49:47 +0100

    update

--- src/main/java/pl/training/shop/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -55,6 +56,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
         AuthoritiesAuthorizationManager authoritiesAuthorizationManager; // Jedna z implementacji AuthorizationManager (role)*/
 
 // @EnableWebSecurity(debug = true)
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true/*, prePostEnabled = true*/)
 @Configuration
 public class SecurityConfiguration {
 
--- src/main/java/pl/training/shop/payments/adapters/rest/PaymentProcessorRestController.java
+++ src/main/java/pl/training/shop/payments/adapters/rest/PaymentProcessorRestController.java
@@ -2,6 +2,10 @@ package pl.training.shop.payments.adapters.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,13 @@ public class PaymentProcessorRestController {
     private final PaymentProcessor paymentProcessor;
     private final RestPaymentModelMapper mapper;
 
+
+    // @Secured("ROLE_MANAGER")
+    // @RolesAllowed("MANAGER")
+    // @PreAuthorize("#paymentRequestDto.id > 100")
+    // @PostAuthorize("returnObject.statusCode != 201")
+    // @PreFilter("filterObject.owner == authentication.name")
+    // @PostFilter("filterObject.owner == authentication.name")
     @PostMapping
     public ResponseEntity<PaymentDto> process(/*@Valid*/ @Validated(Base.class) @RequestBody PaymentRequestDto paymentRequestDto) {
         var paymentRequest = mapper.toDomain(paymentRequestDto);
